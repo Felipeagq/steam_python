ccd0a77ffba1e8198ed89cf4550399f4f79d9d3b
Felipeagq <felipeagq99@gmail.com>
2023-02-24 19:52:14 -0500

    update

---
+++ ejercicios_list_dict/dict_exp.py
@@ -0,0 +1,18 @@
+diccionario = {"nombre":"Felipe"}
+diccionario["apellido"] = "gonzalez"
+temp = {"edad":23,
+        "sexo":"M",
+        "nombre":"gabriela",
+        "Profesion":"ing sistemas"}
+diccionario.update(temp)
+print(diccionario)
+
+# diccionario["extra"] = temp
+# diccionario["extra"].pop("sexo")
+# diccionario["extra"]["Profesion"] = ["Profesor","ing","bailarin","modelo","batman"]
+
+# diccionario["extra"]["Profesion"].append("astronauta")
+# print(diccionario["extra"]["Profesion"])
+# print(diccionario["extra"]["Profesion"])
+
+
