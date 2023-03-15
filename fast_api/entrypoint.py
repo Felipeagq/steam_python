4447cb43c71ba76a672a4c867a87173783b3bdc0
Felipeagq <felipeagq99@gmail.com>
2023-03-14 20:52:01 -0500

    [ADD] biblioteca

--- fast_api/entrypoint.py
+++ fast_api/entrypoint.py
@@ -2,51 +2,77 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-biblioteca = {
-    "1":{
-        "nombre":"felipe",
-        "edad":"23",
-        "libros":[
-            {
-            "libro":"5am",
-            "fecha":"13/3/2023",
-            "estado":"prestado"
-        },
-            {
-            "libro":"100 años de soledad",
-            "fecha":"13/5/2023",
-            "estado":"prestado"
-        },
-        ]
-    },
-    "2":{
-        "nombre":"Sharid",
-        "edad":"23",
-        "libros":[
-            {
-            "libro":"habtios atomicos",
-            "fecha":"13/3/2023",
-            "estado":"prestado"
-        },
-            {
-            "libro":"poesia y mitos",
-            "fecha":"13/5/2023",
-            "estado":"prestado"
-        },
-        ]
-    }
-}
-
+biblioteca = []
 
 # registramos una función en mi app en ruta (endpoint)
 # "/" ruta raiz
 # Solo podemos tener una función por verbo en una ruta
-@app.get("/{id}")
-def hola(id:str):
-    return biblioteca[id]
+@app.get("/")
+def hello_world_check():
+    return  {
+        "titulo":"bilioteca STEAM",
+        "version":"v0.0.1"
+    }
 
-@app.get("/hola/{nombre}/{id}")
-def hola(nombre:str,id:int):
-    return f"hola {nombre}"          
+
+@app.get("/personas")
+def personas_all():
+    """Me regresa todas las personas 
+    registrada en mi biblioteca
+
+    Returns:
+        biblioteca [list]
+    """
+    return biblioteca
+
+@app.get("/personas/{id}")
+def personas_one(id:str):
+    """Me retorno la persona con un id
+
+    Args:
+        id (str): El id de la persona
+
+    Returns:
+        bilioteca[id] [dict]: es la información basica
+                                de la persona
+    """
+    for i in biblioteca:
+        if i.id == id:
+            print(i.id)
+            return i
+    return {"error":"Persona no encontrada"}
+
+from pydantic import BaseModel
+class PersonaBiblioteca(BaseModel):
+    id:str
+    nombre:str
+    edad:int
+    libros:dict
+@app.post("/personas")
+def personas_add(request:PersonaBiblioteca):
+    biblioteca.append(request)
+    return request
+
+
+class PersonaModify(BaseModel):
+    id:str
+    nombre:str
+    edad:int
+@app.put("/persona")
+def personas_modify(request:PersonaModify):
+    for i in biblioteca:
+        print(i)
+        if i.id == request.id:
+            i.nombre = request.nombre
+            i.edad = request.edad
+            return i
+    return {"error":"Persona no encontrada"}
+
+
+@app.delete("/personas/{id}")
+def personas_delete(id:str):
+    for i in biblioteca:
+        if i.id == id:
+            print(i.id)
+            return i
+    return {"error":"Persona no encontrada"}
